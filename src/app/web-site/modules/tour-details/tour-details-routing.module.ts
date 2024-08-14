e494fbf9d3764342bf464de46d6eae20c43f0135
Omnia Mohamed <74616745+omnia192@users.noreply.github.com>
2024-08-14 14:16:33 +0200

    edit

--- src/app/web-site/modules/tour-details/tour-details-routing.module.ts
+++ src/app/web-site/modules/tour-details/tour-details-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MaindetailComponent } from './maindetail/maindetail.component';
-import { ActivityditailComponent } from './activityditail/activityditail.component';
 
 const routes: Routes = [
 
   {path:'',component:MaindetailComponent},
-    {path:'trip/:typeid', component: ActivityditailComponent}
 
-  // {path:'',component:MaindetailComponent,children:[
-  //   {path:'trip/:typeid', component: ActivityditailComponent}
-  // ]}
+
 ];
 
 @NgModule({
