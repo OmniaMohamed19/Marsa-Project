f7df07d80c0acdae830b5dd25b5c2df5a036d9a9
Omnia Mohamed <74616745+omnia192@users.noreply.github.com>
2024-08-15 14:13:19 +0200

    edit

--- src/app/web-site/modules/tour-details/tour-details.module.ts
+++ src/app/web-site/modules/tour-details/tour-details.module.ts
@@ -1,20 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 import { TourDetailsRoutingModule } from './tour-details-routing.module';
 import { MaindetailComponent } from './maindetail/maindetail.component';
 import { CarouselModule } from 'primeng/carousel';
+import { DropdownModule } from 'primeng/dropdown';
 
 @NgModule({
+
   declarations: [
     MaindetailComponent,
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA],
   imports: [
     CommonModule,
     TourDetailsRoutingModule,
     SharedModule,
-    CarouselModule
+    CarouselModule,
+    DropdownModule
 
   ]
 })
