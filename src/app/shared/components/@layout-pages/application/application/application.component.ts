988064cc1434f1e89d9b008da61bdf1f67ee8e82
omnia192 <omnia.mohamed.4600@gmail.com>
2025-03-11 17:32:50 +0200

    edit

--- src/app/shared/components/@layout-pages/application/application/application.component.ts
+++ src/app/shared/components/@layout-pages/application/application/application.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpService } from 'src/app/core/services/http/http.service';
 import { environment } from 'src/environments/environment.prod';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-application',
@@ -11,16 +13,18 @@ export class ApplicationComponent {
   details: any;
   mobile_description: any;
   mobile_image: any;
-  constructor(private httpService: HttpService) {}
+  constructor(private httpService: HttpService,private sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
     this.httpService.get(environment.marsa, 'Coupon').subscribe((res: any) => {
       this.details = res?.coupon;
     });
     this.httpService.get(environment.marsa, 'Background').subscribe((res: any) => {
-      this.mobile_description = res?.mobile_section_text;
-      console.log( this.mobile_description)
-     this.mobile_image=res?.mobile_section;
+      if (res?.mobile_section_text) {
+        console.log(res.mobile_section_text);
+        this.mobile_description = this.sanitizer.bypassSecurityTrustHtml(res.mobile_section_text);
+      }
+      this.mobile_image = res?.mobile_section;
     });
   }
 }
