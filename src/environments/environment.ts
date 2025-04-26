f14f85252c3b2b76136476b22b8d8473dec61103
omnia192 <omnia.mohamed.4600@gmail.com>
2025-04-26 19:41:42 +0200

    edit environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  APIURL:"https://marvelits.com/api/",
+  APIURL:"https:///Wavuni.com/api/",
   marsa: 'marsa',
   googleClientId: '530644158352-38j40k29rjl74b7t2re9llq4n1om8gdn.apps.googleusercontent.com',
   facebookAppId: 'YOUR_FACEBOOK_APP_ID',
